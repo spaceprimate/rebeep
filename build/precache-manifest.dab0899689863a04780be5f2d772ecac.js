c1d7d12b775e4c541d90b68e999d0e54b26f0ee1
dtmurphy <dtmurphy@uno.edu>
2021-01-22 22:24:06 -0600

    added build

---
+++ build/precache-manifest.dab0899689863a04780be5f2d772ecac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7a828c5c4fc6cd13c29eb22eb5cab94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2325b049dd5ac477cbe7",
+    "url": "/static/css/main.563979d4.chunk.css"
+  },
+  {
+    "revision": "8d67156d6b89ea8a0f36",
+    "url": "/static/js/2.53ca39bd.chunk.js"
+  },
+  {
+    "revision": "34eb3dfc071d8c66f44fd18d73daeb25",
+    "url": "/static/js/2.53ca39bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2325b049dd5ac477cbe7",
+    "url": "/static/js/main.de0d3b85.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "bc05e42521ccadac2527da94bb06cf8f",
+    "url": "/static/media/beep.bc05e425.png"
+  }
+]);
