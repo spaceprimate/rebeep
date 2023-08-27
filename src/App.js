643a1e56c12a738762e3cc9ae09851d3fd8f584b
dtmurphy <dtmurphy@uno.edu>
2023-08-27 14:59:27 -0500

    updated pad interaction

--- src/App.js
+++ src/App.js
@@ -755,47 +755,73 @@ class Chaos extends React.Component{
         super(props);
         this.state = {
             active: false,
+            top: 0,
+            left: 0,
         };
 
         this.mouseDown = false;
 
     }
     _onMouseDown(e){
-        this.mouseDown = true;
+        window.mouseDown = true;
+        this.setState({top: e.nativeEvent.offsetY, left: e.nativeEvent.offsetX});
     }
     _onMouseUp(e){
-        this.mouseDown = false;
+      console.log('------------ mouse up called')
+        window.mouseDown = false;
     }
     _onMouseOut(e){
         this.mouseDown=false;
     }
+
 
     
 
 
     _onMouseMove(e) {
         // this.setState({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
-        if(this.mouseDown){
+
+        console.log(window.mouseDown)
+        if(window.mouseDown){
             // console.log(e.nativeEvent.offsetX);
-            this.props.setChaos(e.nativeEvent.offsetX, (-1 * (e.nativeEvent.offsetY - 255)));
             
+            this.props.setChaos(e.nativeEvent.offsetX, (-1 * (e.nativeEvent.offsetY - 255)));
+            this.setState({top: e.nativeEvent.offsetY, left: e.nativeEvent.offsetX});
+            // this.top = e.nativeEvent.offsetY;
+            // this.left = e.nativeEvent.offsetX;
+            // console.log(e.nativeEvent)
         }
+        // else{
+        //   console.log("mouse is not down")
+        // }
         
       }
+
+      pointStyle = {
+        
+      };
 
       
 
 
 
     render() {
+     
+      let that = this;
         return (
             <>
+            <div style={{color:'red'}}>
+            {that.top !== undefined ? that.top.toString() : 'bob'}, {this.left}
+            {this.state.top.toString() + ", " + this.state.left.toString()}
+            </div>
+            
                 <div className="chaos-pad"  
                     onMouseMove={this._onMouseMove.bind(this)} 
                     onMouseDown={this._onMouseDown.bind(this)}
                     onMouseUp={this._onMouseUp.bind(this)}
                     onMouseOut={this._onMouseOut.bind(this)}
                 >
+                  <div className="chaos-point" style={{top:`${this.state.top}px`, left: `${this.state.left}px`}}></div>
 
                 </div>
             </>
